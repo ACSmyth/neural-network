1cd2f0d5c15a4d19d89e4658dfe43d93515bfef4
ACSmyth <alex@smythclan.com>
2020-08-13 14:20:24 -0400

    Add genetic algorithm for non-adversarial games

--- main.py
+++ main.py
@@ -102,8 +102,89 @@ class NeuralNetwork:
 					ret.layers[i].neurons[j].weights[k] = self.layers[i].neurons[j].weights[k]
 		return ret
 
-
 class GeneticAlgorithm:
+	# dimensions: list of ints
+	# population_size: int
+	# input_func: game state -> neural network input array
+	# run_game_func: agent1, agent2, input_func -> final game state
+	# fitness_func: final game state -> two element list of fitness of both agents
+	def __init__(self, dimensions, population_size, rounds_per_agent, input_func, run_game_func, fitness_func):
+		self.dimensions = dimensions
+		self.population_size = population_size
+		self.rounds_per_agent = rounds_per_agent
+		self.input_func = input_func
+		self.run_game_func = run_game_func
+		self.fitness_func = fitness_func
+		self.gen_count = 0
+		self.population = []
+		self.best_agent = None
+
+	def run_generation(self):
+		if self.gen_count == 0:
+			# generate initial population pool
+			for i in range(self.population_size):
+				# [net, avg fitness, num trials]
+				self.population.append([NeuralNetwork(self.dimensions), 0, 0])
+
+		# reset old fitness - although reset most in prev generation
+		for agent in self.population:
+			agent[1], agent[2] = 0, 0
+
+		best_agents = self.population[:self.num_opponents]
+		cloned_best_agents = [[agent[0].deep_clone(), agent[1], agent[2]] for agent in best_agents]
+
+		for agent in self.population:
+			net = agent[0]
+
+			# everyone plays by themselves
+			for i in range(self.rounds_per_agent):
+				final_game_state = self.run_game_func(net, opponent_net, self.input_func)
+				fitness = self.fitness_func(final_game_state)
+				avg_fitness += fitness
+			avg_fitness /= self.rounds_per_agent
+			agent[1] = avg_fitness
+			agent[2] = self.rounds_per_agent
+
+		# select / merge
+		self.population.sort(key=lambda p: p[1], reverse=True) # sort by fitness, descending order
+		self.best_agent = self.population[0]
+
+		# randomly pick from pop -> best more likely
+		def ran_idx():
+			return int(4 * (-math.log(-random.random() + 1) + 0.1))
+
+		new_population = []
+		for i in range(len(self.population) - self.num_opponents):
+			i1 = ran_idx()
+			i2 = ran_idx()
+			if i1 >= len(self.population):
+				i1 = 0
+			if i2 >= len(self.population):
+				i2 = 0
+			merged = self.population[i1][0].merge(self.population[i2][0])
+			new_population.append([merged, 0, 0])
+
+
+		# mutate
+		for agent in new_population:
+			agent[0].mutate()
+
+		# add best agents from prev generation unmutated
+		new_population.extend(cloned_best_agents)
+
+		self.population.clear()
+		self.population.extend(new_population)
+
+		self.gen_count += 1
+
+	def get_best_agent(self):
+		return self.best_agent
+
+
+
+
+
+class AdversarialGeneticAlgorithm:
 	# dimensions: list of ints
 	# population_size: int
 	# input_func: game state -> neural network input array
