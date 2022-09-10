597ce46f25a4da9aea593b35e1dcd1761601eb25
ACSmyth <smyth.a@northeastern.edu>
2022-09-10 17:52:23 -0400

    Add test for matrix approach

---
+++ test.py
@@ -0,0 +1,20 @@
+import unittest
+import numpy as np
+from neural_network import NeuralNetwork
+from hypothesis import given, strategies as st
+
+class Tests(unittest.TestCase):
+  @given(st.integers(min_value=0, max_value=10**5))
+  def test_neural_network_multiplies(self, n):
+    nn = NeuralNetwork((2,3,1))
+    nn.layers[0].weights_matrix = np.zeros((2,3))
+    nn.layers[0].weights_matrix[0,0] = n
+    nn.layers[0].biases_matrix = np.zeros(3)
+    nn.layers[1].weights_matrix = np.zeros(3)
+    nn.layers[1].weights_matrix[0] = n
+    nn.layers[1].biases_matrix = np.zeros(1)
+    out = nn.forward_propagate([n, 0])
+
+    return self.assertEqual(out[0], n**3)
+
+unittest.main()
